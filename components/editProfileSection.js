dad0536d13fbde418183d48e6c19ddf05c30d47f
Lorenze Jay <Lorenzejay@gmail.com>
2021-03-09 06:31:36 -0800

    created section for top programming languages, enabled disabling of checkboxes after 3 were chosen

--- components/editProfileSection.js
+++ components/editProfileSection.js
@@ -2,15 +2,11 @@ import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import { useAuth } from "../hooks/useAuth";
 import { db, storage } from "../utils/firebase";
-// const updateImage = async (file) => {
-//     const snapShot = storage.ref().child('images').put(file)
-//     setImage
-// }
 
 const EditProfileSection = () => {
   const router = useRouter();
   const auth = useAuth();
-  const { user, updateAbout, updateProfilePic } = auth;
+  const { user, updateAbout, updateProfilePic, updateTopThreeLanguages } = auth;
 
   //pulled from the db
   const [aboutMe, setAboutMe] = useState("this is it");
@@ -18,6 +14,7 @@ const EditProfileSection = () => {
   const [toSendPP, setToSendPP] = useState("");
   const [profilePic, setProfilePic] = useState("");
   const [profilePicPreview, setProfilePicPreview] = useState("");
+  const [top3Languages, setTopThreeLanguagas] = useState([]);
   const [errors, setErrors] = useState();
 
   //make sure there is a logged in user
@@ -86,6 +83,46 @@ const EditProfileSection = () => {
     updateAbout(aboutMe);
   };
 
+  const [checked, setChecked] = useState({});
+  const checkedCount = Object.keys(checked).filter((key) => checked[key]).length;
+  const disabled = checkedCount > 2;
+
+  const handleSelectLanguages = (i, e) => {
+    const { value } = e.target;
+    // console.log(!checked[i]);
+
+    if (!checked[i] === true) {
+      setTopThreeLanguagas([...top3Languages, { language: value }]);
+    } else {
+      const filteredUnchecked = top3Languages.filter((x) => x.language !== value);
+
+      setTopThreeLanguagas(filteredUnchecked);
+    }
+    setChecked((prevState) => ({
+      ...prevState,
+      [i]: !checked[i],
+    }));
+  };
+  console.log(checked);
+  console.log(top3Languages);
+
+  const [programmingLanguages] = useState([
+    "Java",
+    "JavaScript",
+    "Python",
+    "Swift",
+    "React",
+    "Angular",
+  ]);
+
+  const handleUpdateLanguages = () => {
+    if (top3Languages.length > 0) {
+      updateTopThreeLanguages(top3Languages);
+      return window.alert("Successfully updated your top 3 languages");
+    }
+    window.alert("You must have at least one language selected.");
+  };
+
   return (
     <section className="text-gray-400 mb-10">
       <div>
@@ -101,7 +138,7 @@ const EditProfileSection = () => {
           onChange={handleSettingProfileImage}
         />
         <button
-          className={`bg-gray-800 w-full disabled:opacity-50 text-white transition duration-500 ease-in-out`}
+          className={`bg-gray-800 w-full disabled:opacity-50 text-white transition duration-500 ease-in-out px-3 py-1`}
           disabled={!profilePicPreview}
           onClick={handleUpdateProfilePicture}
         >
@@ -123,12 +160,44 @@ const EditProfileSection = () => {
             {150 - aboutCharCount}
           </p>
           <button
-            className={`bg-gray-800 w-full disabled:opacity-50 text-white transition duration-500 ease-in-out`}
+            className={`bg-gray-800 w-full disabled:opacity-50 text-white transition duration-500 ease-in-out px-5 py-1`}
             disabled={150 - aboutCharCount < 0 ? true : false}
             onClick={handleUpdateAboutMe}
           >
             Update
           </button>
+        </div>
+        <div className="rounded-lg border border-gray-400 flex flex-col p-3 my-4 text-gray-800">
+          My Top 3 Programming Languages
+          <div>
+            {programmingLanguages.map((language, i) => {
+              return (
+                <div>
+                  <input
+                    type="checkbox"
+                    name={language}
+                    value={language}
+                    className="mr-4"
+                    onChange={(e) => handleSelectLanguages(i, e)}
+                    checked={checked[i] || false}
+                    disabled={!checked[i] && disabled}
+                  />
+                  <label
+                    htmlFor={language}
+                    className={`${!checked[i] ? "text-gray-400" : "text-gray-800"}`}
+                  >
+                    {language}
+                  </label>
+                </div>
+              );
+            })}
+            <button
+              onClick={handleUpdateLanguages}
+              className="mt-3 px-5 py-1 bg-gray-800 text-white"
+            >
+              Update
+            </button>
+          </div>
         </div>
       </div>
     </section>
